cb7bb4d59ba9d70b9959f41b708c66a53387572b
Аббас Гусенов <gusenov@live.ru>
2018-04-11 00:24:57 +0600

    NON-EXECUTABLE: /etc/cron.weekly/apt-xapian-index

--- optimize.sh
+++ optimize.sh
@@ -11,3 +11,14 @@ kwriteconfig --file ~/.config/plasmarc --group PlasmaToolTips --key Delay -- -1
 # https://help.ubuntu.com/community/AutomaticSecurityUpdates
 sudo sed -i 's/APT::Periodic::Update-Package-Lists "1";/APT::Periodic::Update-Package-Lists "0";/g' "/etc/apt/apt.conf.d/20auto-upgrades"
 sudo sed -i 's/APT::Periodic::Unattended-Upgrade "1";/APT::Periodic::Unattended-Upgrade "0";/g' "/etc/apt/apt.conf.d/20auto-upgrades"
+
+# https://askubuntu.com/questions/79481/is-100-cpu-usage-harmful-while-update-apt-xapi-runs
+# https://ubuntuforums.org/showthread.php?t=1062688
+# http://forum.ubuntu.ru/index.php?topic=100553.0
+# http://forum.ubuntu.ru/index.php?topic=257553.0
+sudo chmod a-x /etc/cron.weekly/apt-xapian-index
+#sudo chmod 644 /etc/cron.weekly/apt-xapian-index
+# $ stat /etc/cron.weekly/apt-xapian-index
+# Access: (0644/-rw-r--r--)  Uid: (    0/    root)   Gid: (    0/    root)
+# $ ls -la /etc/cron.weekly/
+# -rw-r--r--   1 root root   730 Apr 14  2016 apt-xapian-index
