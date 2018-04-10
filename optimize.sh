ab220376169382bb4c92326378325f35ba2e62c7
Аббас Гусенов <gusenov@live.ru>
2018-04-11 00:56:40 +0600

    DISABLED: Akonadi start automatically

--- optimize.sh
+++ optimize.sh
@@ -7,6 +7,10 @@ set -x # echo on
 # http://shitohichiumaya.blogspot.com/2017/01/how-to-workaround-frequently-crash.html
 kwriteconfig --file ~/.config/plasmarc --group PlasmaToolTips --key Delay -- -1
 
+# https://ubuntuforums.org/showthread.php?t=1764048&s=b7420bf8119f62ca810b3a658cccc284&p=10881947#post10881947
+# https://askubuntu.com/questions/453422/removing-nepomuk-and-akonadi
+kwriteconfig --file ~/.config/akonadi/akonadiserverrc --group QMYSQL --key StartServer -- false
+
 # https://askubuntu.com/questions/798989/heavy-load-by-apt-unattended-upgrade
 # https://help.ubuntu.com/community/AutomaticSecurityUpdates
 sudo sed -i 's/APT::Periodic::Update-Package-Lists "1";/APT::Periodic::Update-Package-Lists "0";/g' "/etc/apt/apt.conf.d/20auto-upgrades"
