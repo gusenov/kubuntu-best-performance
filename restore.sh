c972216975ddf738128415456b0a1e01a364ccae
Аббас Гусенов <gusenov@live.ru>
2018-04-10 23:47:10 +0600

    ENABLED: Automatic Security Updates

--- restore.sh
+++ restore.sh
@@ -5,3 +5,6 @@ set -x # echo on
 #  $ ./restore.sh
 
 kwriteconfig --file ~/.config/plasmarc --group PlasmaToolTips --key Delay -- 0.69999999999999996
+
+sudo sed -i 's/APT::Periodic::Update-Package-Lists "0";/APT::Periodic::Update-Package-Lists "1";/g' "/etc/apt/apt.conf.d/20auto-upgrades"
+sudo sed -i 's/APT::Periodic::Unattended-Upgrade "0";/APT::Periodic::Unattended-Upgrade "1";/g' "/etc/apt/apt.conf.d/20auto-upgrades"
