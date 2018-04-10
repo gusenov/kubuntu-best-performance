8d28605391560d5d540324e70fe57d97a58a02b2
Аббас Гусенов <gusenov@live.ru>
2018-04-11 00:24:12 +0600

    EXECUTABLE: /etc/cron.weekly/apt-xapian-index

--- restore.sh
+++ restore.sh
@@ -8,3 +8,10 @@ kwriteconfig --file ~/.config/plasmarc --group PlasmaToolTips --key Delay -- 0.6
 
 sudo sed -i 's/APT::Periodic::Update-Package-Lists "0";/APT::Periodic::Update-Package-Lists "1";/g' "/etc/apt/apt.conf.d/20auto-upgrades"
 sudo sed -i 's/APT::Periodic::Unattended-Upgrade "0";/APT::Periodic::Unattended-Upgrade "1";/g' "/etc/apt/apt.conf.d/20auto-upgrades"
+
+sudo chmod a+x /etc/cron.weekly/apt-xapian-index
+#sudo chmod 755 /etc/cron.weekly/apt-xapian-index
+# $ stat /etc/cron.weekly/apt-xapian-index
+# Access: (0755/-rwxr-xr-x)  Uid: (    0/    root)   Gid: (    0/    root)
+# $ ls -la /etc/cron.weekly/
+# -rwxr-xr-x   1 root root   730 Apr 14  2016 apt-xapian-index
