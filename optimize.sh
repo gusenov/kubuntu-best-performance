ab257a50e2b04591ace17ef1c6f7e514f6cf9c18
Аббас Гусенов <gusenov@live.ru>
2018-04-11 13:33:18 +0600

    DISABLED: System Settings ⇒ Desktop Effects ⇒ Appearance
    - Fade (Make windows smoothly fade in and out when they are shown or hidden)
    - Login (Smoothly fade to the desktop when logging in)
    - Logout (Desature the desktop when displaying the logout dialog)
    - Maximize (Animation for a window going to maximize/restore from maximize)
    - Screen Edge (Highlights a screen edge when approaching)
    - Sliding popups (Sliding animation for Plasma popups)
    - Translucency (Make windows translucent under different conditions)
    - Window Aperture (Move window into the corners while showing desktop)
    - Minimize Animation (Animate the minimizing of windows)

--- optimize.sh
+++ optimize.sh
@@ -9,6 +9,17 @@ kwriteconfig --file ~/.config/plasmarc --group PlasmaToolTips --key Delay -- -1
 
 kwriteconfig --file ~/.config/kwinrc --group Plugins --key zoomEnabled --type bool -- false
 
+# System Settings ⇒ Desktop Effects ⇒ Appearance
+kwriteconfig --file ~/.config/kwinrc --group Plugins --key kwin4_effect_fadeEnabled --type bool -- false
+kwriteconfig --file ~/.config/kwinrc --group Plugins --key kwin4_effect_loginEnabled --type bool -- false
+kwriteconfig --file ~/.config/kwinrc --group Plugins --key logoutEnabled --type bool -- false
+kwriteconfig --file ~/.config/kwinrc --group Plugins --key kwin4_effect_maximizeEnabled --type bool -- false
+kwriteconfig --file ~/.config/kwinrc --group Plugins --key screenedgeEnabled --type bool -- false
+kwriteconfig --file ~/.config/kwinrc --group Plugins --key slidingpopupsEnabled --type bool -- false
+kwriteconfig --file ~/.config/kwinrc --group Plugins --key kwin4_effect_translucencyEnabled --type bool -- false
+kwriteconfig --file ~/.config/kwinrc --group Plugins --key kwin4_effect_windowapertureEnabled --type bool -- false
+kwriteconfig --file ~/.config/kwinrc --group Plugins --key minimizeanimationEnabled --type bool -- false
+
 # https://ubuntuforums.org/showthread.php?t=1764048&s=b7420bf8119f62ca810b3a658cccc284&p=10881947#post10881947
 # https://askubuntu.com/questions/453422/removing-nepomuk-and-akonadi
 kwriteconfig --file ~/.config/akonadi/akonadiserverrc --group QMYSQL --key StartServer -- false
