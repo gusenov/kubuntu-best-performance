d0c8c55daa5ec2eef726f6a17b58c502cac97948
Аббас Гусенов <gusenov@live.ru>
2018-04-11 13:45:46 +0600

    ENABLED: Slide (Slide windows across the screen when switching virtual desktops)

--- restore.sh
+++ restore.sh
@@ -21,6 +21,8 @@ kwriteconfig --file ~/.config/kwinrc --group Plugins --key minimizeanimationEnab
 
 kwriteconfig --file ~/.config/kwinrc --group Plugins --key kwin4_effect_dialogparentEnabled --type bool -- true
 
+kwriteconfig --file ~/.config/kwinrc --group Plugins --key slideEnabled --type bool -- true
+
 kwriteconfig --file ~/.config/akonadi/akonadiserverrc --group QMYSQL --key StartServer -- true
 
 sudo sed -i 's/APT::Periodic::Update-Package-Lists "0";/APT::Periodic::Update-Package-Lists "1";/g' "/etc/apt/apt.conf.d/20auto-upgrades"
