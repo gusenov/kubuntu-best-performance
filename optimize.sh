a735c281cb6694a8ec9477e8465c11a9744b5b11
Аббас Гусенов <gusenov@live.ru>
2018-04-11 13:49:21 +0600

    DISABLED: Slide (Slide windows across the screen when switching virtual desktops)

--- optimize.sh
+++ optimize.sh
@@ -22,6 +22,8 @@ kwriteconfig --file ~/.config/kwinrc --group Plugins --key minimizeanimationEnab
 
 kwriteconfig --file ~/.config/kwinrc --group Plugins --key kwin4_effect_dialogparentEnabled --type bool -- false
 
+kwriteconfig --file ~/.config/kwinrc --group Plugins --key slideEnabled --type bool -- false
+
 # https://ubuntuforums.org/showthread.php?t=1764048&s=b7420bf8119f62ca810b3a658cccc284&p=10881947#post10881947
 # https://askubuntu.com/questions/453422/removing-nepomuk-and-akonadi
 kwriteconfig --file ~/.config/akonadi/akonadiserverrc --group QMYSQL --key StartServer -- false
