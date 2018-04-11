af813b9dc0d1fc485080d2060862e7161980b270
Аббас Гусенов <gusenov@live.ru>
2018-04-11 13:28:31 +0600

    ENABLED: System Settings ⇒ Desktop Effects ⇒ Appearance
    - Fade (Make windows smoothly fade in and out when they are shown or hidden)
    - Login (Smoothly fade to the desktop when logging in)
    - Logout (Desature the desktop when displaying the logout dialog)
    - Maximize (Animation for a window going to maximize/restore from maximize)
    - Screen Edge (Highlights a screen edge when approaching)
    - Sliding popups (Sliding animation for Plasma popups)
    - Translucency (Make windows translucent under different conditions)
    - Window Aperture (Move window into the corners while showing desktop)
    - Minimize Animation (Animate the minimizing of windows)

--- restore.sh
+++ restore.sh
@@ -8,6 +8,17 @@ kwriteconfig --file ~/.config/plasmarc --group PlasmaToolTips --key Delay -- 0.6
 
 kwriteconfig --file ~/.config/kwinrc --group Plugins --key zoomEnabled --type bool -- true
 
+# System Settings ⇒ Desktop Effects ⇒ Appearance
+kwriteconfig --file ~/.config/kwinrc --group Plugins --key kwin4_effect_fadeEnabled --type bool -- true
+kwriteconfig --file ~/.config/kwinrc --group Plugins --key kwin4_effect_loginEnabled --type bool -- true
+kwriteconfig --file ~/.config/kwinrc --group Plugins --key logoutEnabled --type bool -- true
+kwriteconfig --file ~/.config/kwinrc --group Plugins --key kwin4_effect_maximizeEnabled --type bool -- true
+kwriteconfig --file ~/.config/kwinrc --group Plugins --key screenedgeEnabled --type bool -- true
+kwriteconfig --file ~/.config/kwinrc --group Plugins --key slidingpopupsEnabled --type bool -- true
+kwriteconfig --file ~/.config/kwinrc --group Plugins --key kwin4_effect_translucencyEnabled --type bool -- true
+kwriteconfig --file ~/.config/kwinrc --group Plugins --key kwin4_effect_windowapertureEnabled --type bool -- true
+kwriteconfig --file ~/.config/kwinrc --group Plugins --key minimizeanimationEnabled --type bool -- true
+
 kwriteconfig --file ~/.config/akonadi/akonadiserverrc --group QMYSQL --key StartServer -- true
 
 sudo sed -i 's/APT::Periodic::Update-Package-Lists "0";/APT::Periodic::Update-Package-Lists "1";/g' "/etc/apt/apt.conf.d/20auto-upgrades"
