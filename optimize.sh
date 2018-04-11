9668bc8296244bfa632e49961506e27dfdcd7364
Аббас Гусенов <gusenov@live.ru>
2018-04-11 13:43:56 +0600

    DISABLED: Dialog Parent (Darkens the parent window of the currently active dialog)

--- optimize.sh
+++ optimize.sh
@@ -20,6 +20,8 @@ kwriteconfig --file ~/.config/kwinrc --group Plugins --key kwin4_effect_transluc
 kwriteconfig --file ~/.config/kwinrc --group Plugins --key kwin4_effect_windowapertureEnabled --type bool -- false
 kwriteconfig --file ~/.config/kwinrc --group Plugins --key minimizeanimationEnabled --type bool -- false
 
+kwriteconfig --file ~/.config/kwinrc --group Plugins --key kwin4_effect_dialogparentEnabled --type bool -- false
+
 # https://ubuntuforums.org/showthread.php?t=1764048&s=b7420bf8119f62ca810b3a658cccc284&p=10881947#post10881947
 # https://askubuntu.com/questions/453422/removing-nepomuk-and-akonadi
 kwriteconfig --file ~/.config/akonadi/akonadiserverrc --group QMYSQL --key StartServer -- false
